37a9bce152e6ccdca340b86291b129dc5c1f89af
Amit PR <aspper20@gmail.com>
2021-03-12 00:24:46 +0530

    Filters applied on category, glass and ingredients

--- src/sections/cocktail-container.js
+++ src/sections/cocktail-container.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import CocktailList from '../components/cocktail-list';
+import FilteredCocktails from '../components/filtered-cocktails';
 import Footer from '../components/footer';
 import Header from '../components/header';
 import axios from 'axios';
@@ -9,10 +10,27 @@ class CocktailSection extends Component {
     super();
     this.state = {
       notFound: false,
+      cocktails: null,
+      categories: null,
+      ingredients: null,
+      glasses: null,
+      filterResults: null,
     }
 
     this.getCocktails = this.getCocktails.bind(this);
+    this.getIngredients = this.getIngredients.bind(this);
+    this.getCategories = this.getCategories.bind(this);
+    this.getGlasses = this.getGlasses.bind(this);
     this.clearSearch = this.clearSearch.bind(this);
+    this.filterByCategory = this.filterByCategory.bind(this);
+    this.filterByGlass = this.filterByGlass.bind(this);
+    this.filterByIngredient = this.filterByIngredient.bind(this);
+  }
+
+  componentDidMount() {
+    this.getCategories();
+    this.getIngredients();
+    this.getGlasses();
   }
 
   async getCocktails(searchText) {
@@ -26,19 +44,75 @@ class CocktailSection extends Component {
     })
   }
 
-  clearSearch(searchText) {
+  async getIngredients() {
+    const ingredientsData = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list');
     this.setState({
-      cocktails: null
+      ingredients: ingredientsData.data.drinks,
+    })
+  }
+
+  async getCategories() {
+    const categoryData = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+    this.setState({
+      categories: categoryData.data.drinks,
+    })
+  }
+
+  async getGlasses() {
+    const glassesData = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list');
+    this.setState({
+      glasses: glassesData.data.drinks,
+    })
+  }
+
+  async filterByCategory(categoryName) {
+    const filteredData = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=' + categoryName);
+    this.setState({
+      filterResults: filteredData.data.drinks
+    })
+  }
+
+  async filterByGlass(glassName) {
+    const filteredData = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=' + glassName);
+    this.setState({
+      filterResults: filteredData.data.drinks
+    })
+  }
+
+  async filterByIngredient(ingredientName) {
+    const filteredData = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + ingredientName);
+    this.setState({
+      filterResults: filteredData.data.drinks
+    })
+  }
+
+  clearSearch() {
+    this.setState({
+      cocktails: null,
+      filterResults: null,
     })
   }
 
   render() {
-    const { cocktails, notFound } = this.state;
+    const { cocktails, notFound, categories, glasses, ingredients, filterResults } = this.state;
     return (
       <Fragment>
-        <Header getCocktails={this.getCocktails} clearSearch={this.clearSearch} />
+        {glasses && categories && ingredients &&
+          <Header getCocktails={this.getCocktails} 
+                  clearSearch={this.clearSearch}
+                  filterByIngredient={this.filterByIngredient}
+                  filterByGlass={this.filterByGlass}
+                  filterByCategory={this.filterByCategory}
+                  categories={categories}
+                  glasses={glasses}
+                  ingredients={ingredients}
+                  />
+        }
         {cocktails &&
           <CocktailList cocktails={cocktails} />
+        }
+        {filterResults &&
+          <FilteredCocktails cocktails={filterResults} />
         }
         {notFound &&
           <h3 className="text-center my-3 text-danger">
